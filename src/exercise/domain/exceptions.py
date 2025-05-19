8db6964c26640824118411f998dd94aa67dadbcc
Adrian Estevez <adrianestevezg@gmail.com>
2025-05-19 15:11:13 +0200

    Red test raise exception with empty exercise name

---
+++ src/exercise/domain/exceptions.py
@@ -0,0 +1,2 @@
+class NameIsEmpty(Exception):
+    pass
--- src/exercise/test/application/test_create_exercise.py
+++ src/exercise/test/application/test_create_exercise.py
@@ -1,4 +1,6 @@
+import pytest
 from exercise.application.create_exercise import CreateExercise, CreateExerciseCommand
+from exercise.domain.exceptions import NameIsEmpty
 from exercise.domain.models import Exercise
 from exercise.domain.repositories import ExercicesRepository
 
@@ -21,6 +23,21 @@ class TestCreateExercise:
                 exercise_name="Example_Exercise"
             )
         )
+        
         exercises = exercise_repository.all()
         assert len(exercises) == 1
         assert exercises[0].name() == "Example_Exercise"
+        
+    
+    def test_raise_exception_when_name_is_empty(self) -> None:
+        exercise_repository = FakeExerciseRepository()
+        
+        with pytest.raises(NameIsEmpty):
+            CreateExercise(exercise_repository).execute(
+                CreateExerciseCommand(
+                    exercise_name=""
+                )
+            )
+        
+        exercices = exercise_repository.all()
+        assert len(exercices) == 0
